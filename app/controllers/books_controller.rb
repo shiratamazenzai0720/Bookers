2b69ab00ed1e24c4ad868473a84621534e3303b7
Ubuntu <ubuntu@ip-172-31-20-1.ap-northeast-1.compute.internal>
2025-09-16 08:27:49 +0000

    [Add]フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,10 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "投稿に成功しました。"
       redirect_to book_path(@book.id)
     else
+      flash.now[:alert] = "投稿に失敗しました。"
       render :new
     end
   end
